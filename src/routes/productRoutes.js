e4d67068a9aa46dbb6954654f0f0bcf03224d0c1
octal <kishanc.octal8@gmail.com>
2026-02-20 18:15:43 +0530

    fix : minor changes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -9,16 +9,17 @@ const {
 
 const { protect } = require('../middlewares/authMiddleware');
 const { authorize } = require('../middlewares/roleMiddleware');
+const upload = require('../middlewares/uploadMiddleware');
 
 const router = express.Router();
 
 router.route('/')
     .get(getProducts)
-    .post(protect, authorize('admin'), createProduct);
+    .post(protect, authorize('admin'), upload.array('images', 5), createProduct);
 
 router.route('/:id')
     .get(getProduct)
-    .put(protect, authorize('admin'), updateProduct)
+    .put(protect, authorize('admin'), upload.array('images', 5), updateProduct)
     .delete(protect, authorize('admin'), deleteProduct);
 
 module.exports = router;
